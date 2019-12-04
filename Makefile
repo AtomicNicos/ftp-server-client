6982f5039e86447528577ca387235bcfeb1f17fb
AtomicNicos <nicolas.boeckh@etu.unige.ch>
2019-12-04 17:38:45 +0100

    Modular Makefile, basic TCP SND/RCV, server loop

--- Makefile
+++ Makefile
@@ -1,12 +1,15 @@
 CC=gcc
 CCFLAGS=-Wall -g
 LDFLAGS=
+
 SOURCES_CLIENT=$(wildcard client/*.c)
 TARGET_CLIENT=ftp-client
 OBJECTS_CLIENT=$(SOURCES_CLIENT:.c=.o)
+
 SOURCES_SERVER=$(wildcard server/*.c)
 TARGET_SERVER=ftp-server
 OBJECTS_SERVER=$(SOURCES_SERVER:.c=.o)
+
 COMPNAME=nicolas_boeckh_ftp
 
 all: $(TARGET_CLIENT) $(TARGET_SERVER)
@@ -17,26 +20,26 @@ $(TARGET_CLIENT): $(OBJECTS_CLIENT)
 $(TARGET_SERVER): $(OBJECTS_SERVER)
 		$(CC) -o server/$@ $^ $(LDFLAGS)
 
-%.o: %.c %.h
+client/%.o: %.c %.h
 		$(CC) $(CCFLAGS) -c $<
 
-%.o: %.c
+server/%.o: %.c %.h
+		$(CC) $(CCFLAGS) -c $<
+
+client/%.o: %.c
+		$(CC) $(CCFLAGS) -c $<
+
+server/%.o: %.c
 		$(CC) $(CCFLAGS) -c $<
 
 clean:
-		rm -f *.o $(TARGET_CLIENT) $(TARGET_SERVER)
+		rm -f */*.o client/$(TARGET_CLIENT) server/$(TARGET_SERVER)
 
 wc: 
-		wc -l *.c *.h
+		wc -l */*.c */*.h
 
 zip:
-		zip $(COMPNAME).zip *.c *.h Makefile README.md
-
-tar:
-		tar -cvzf $(COMPNAME).tar *.c *.h Makefile README.md
+		zip $(COMPNAME).zip */*.c */*.h Makefile README.md
 
 delzip:
 		rm $(COMPNAME).zip
-
-deltar: 
-		rm $(COMPNAME).tar
---
+++ client/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+#include "utils.h"
+
+#define SEND(sock, buffer, max_size, fmt, ...) {\
+memset(buffer, 0, max_size);\
+snprintf(buffer, max_size, fmt, __VA_ARGS__);\
+write(sock, buffer, strlen(buffer)); /* Write to the socket. */\
+}
+
+int main(int argc, char **argv) {
+    struct sockaddr_in serv_addr;
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    int sock, server;
+
+    printf("START\n");
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        FAIL_SUCCESFULLY("Socket could not be created\n");
+
+    if (inet_pton(AF_INET, SERVER_IP, &(serv_addr.sin_addr)) < 0)    // Setup socket connection parameters.
+        FAIL_SUCCESFULLY("INET_ERROR\n");
+    
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+	if ((server = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        FAIL_SUCCESFULLY("CANNOT CREATE SOCKET\n");
+
+    if (connect(server, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0) // Connect to the socket.
+        FAIL_SUCCESFULLY("SOCKET CONNECTION ERROR\n");
+
+    char *buffer = malloc(BUFFER_SIZE);
+    snprintf(buffer, BUFFER_SIZE, "%s", "TEST");
+    int nsent = send(server, buffer, BUFFER_SIZE, 0);
+    printf("SENT %d bytes\n", nsent);
+
+    free(buffer);
+
+    close(sock);
+    close(server);
+    exit(EXIT_SUCCESS);
+}
